020084276f1811feb176c4ca314f30d271f1eaf0
Toleu Bahauddin <baha200477@gmail.com>
2023-08-01 09:28:24 +0600

    update scoring process

--- app/wtask2/repository/repository.py
+++ app/wtask2/repository/repository.py
@@ -75,26 +75,50 @@ class Wtask2Repository:
         chain = load_qa_chain(llm=llm, chain_type="stuff")
 
         evaluator_prompt = f"""
-        firstly check number of words and punctuation,grammar mistakes.
-        "Strict Essay Scoring Criteria for IELTS Evaluation"
-The essay must be scored on a scale of 0.0 to 9.0.
-If the essay response contains fewer than 100 words and 600 characters, the score should be 3.5.
-The evaluation should be based on the IELTS criteria, focusing on four main bands: Task Response, Coherence and Cohesion, Lexical Resource, and Grammar.
-Each band should be scored individually, with clear descriptions provided for each score point in Task Response, Coherence and Cohesion, Lexical Resource, and Grammar.
-The total score for the essay should be calculated as the average of the scores in Task Response, Coherence and Cohesion, Lexical Resource, and Grammar.
-The overall score should be rounded to the nearest 0.5.
-In case the essay contains fewer than 50 words, the score should be 0. For other word count ranges, specific scores should be assigned accordingly.
-The final score should be presented as a number within the range of 0.0 to 9.0, without any additional text or explanations.
-The essay should be evaluated strictly based on the provided criteria for each band, focusing on addressing the task, coherence and organization, vocabulary usage, and grammar accuracy.
-Any additional assessment or subjective comments should be avoided, and the evaluation should remain objective and accurate.
-Example of essay scoring:
+       !IMPORTANT Output only score without Band or The overall score of the essay is.
 
-Task Response: 7.0
-Coherence and Cohesion: 6.5
-Lexical Resource: 7.5
-Grammar: 7.0
-Total score for the IELTS essay: (7.0 + 6.5 + 7.5 + 7.0) / 4 = 7.0
-output ONLY SCORE OF ESSAY DON'T WRITE ANYTHING ELSE
+       1.Firstly check Grammar by this criterias mark essay:
+       Band 1.0: No sentence structure, constant grammar and punctuation errors, unclear communication. 
+       Band 2.0: Basic sentence structures, frequent grammar and punctuation errors, communication often unclear. 
+       Band 3.0: Some simple sentence structures, many grammar and punctuation errors, communication somewhat clear. 
+       Band 4.0: Fair use of simple sentences, several grammar and punctuation errors, communication generally clear.
+       Band 5.0: Limited sentence structures, frequent grammar errors, punctuation issues, causing reader difficulty. 
+       Band 6.0: Uses simple and complex sentences, some grammar and punctuation errors, clear communication. 
+       Band 7.0: Variety of complex sentences, mostly error-free, good grammar control, occasional errors. 
+       Band 8.0: Variety of complex sentences, mostly error-free. 
+       Band 9.0: Expert use of complex sentences, nearly all sentences are error-free, perfect grammar control, flawless punctuation, sophisticated communication.
+       2.Secondly check Lexical Resource.And mark by this criterias:
+       Band 1.0: No vocabulary range, constant spelling errors, unclear communication. 
+       Band 2.0: Limited vocabulary, frequent spelling errors, unclear communication. 
+       Band 3.0: Basic vocabulary range, many spelling errors, communication somewhat clear but often hindered by word formation errors.
+       Band 4.0: Adequate vocabulary, some spelling errors, clear communication. 
+       Band 5.0: Limited vocabulary, frequent spelling errors, causing reader difficulty. 
+       Band 6.0: Adequate vocabulary, some uncommon words with errors, clear communication. 
+       Band 7.0: Sufficient vocabulary, uses less common words, aware of style, occasional errors. 
+       Band 8.0: Wide vocabulary, skillful use of uncommon words, rare errors. Improve by focusing on collocations, careful paraphrasing, topic-appropriate words, correct spelling, avoiding errors and informal language.
+       Band 9.0: Wide vocabulary range, rare spelling errors, precise use of less common words, sophisticated communication.
+       3.Thirty check Coherence and Cohesion by this criterias:
+       Band 1.0: No organization, no use of paragraphs or linking devices, no referencing. 
+       Band 2.0: Minimal organization, improper use of paragraphs and linking devices, unclear referencing. 
+       Band 3.0: Some organization, inconsistent use of paragraphs and linking devices, some referencing. 
+       Band 4.0: Fair organization, uses paragraphs and linking devices with some errors, unclear referencing. 
+       Band 5.0: Some organization, improper use of paragraphs and linking devices, may be repetitive. 
+       Band 6.0: Coherent organization, uses paragraphs and linking devices with some errors, unclear referencing.
+       Band 7.0: Logical organization, good paragraphing, varied linking devices, good referencing. 
+       Band 8.0: Logical organization, appropriate paragraphing, excellent linking and cohesion, flawless referencing.
+       Band 9.0:Excellent organization, perfect paragraphing, skillful use of linking devices, flawless referencing.
+       4.Fourthly check Task Response by this criterias:
+       Band 1.0: Fails to address task, no clear position, no conclusion, irrelevant ideas. 
+       Band 2.0: Barely addresses task, unclear position, no proper conclusion, mostly irrelevant ideas. 
+       Band 3.0: Partially addresses task, vague position, weak conclusion, some relevant ideas. 
+       Band 4.0: Mostly addresses task, somewhat clear position, basic conclusion, relevant but underdeveloped ideas. 
+       Band 5.0: Partial task address, unclear position, possibly no conclusion, underdeveloped ideas, irrelevant details. 
+       Band 6.0: Addresses task, clear position, conclusion may be unclear, relevant but underdeveloped ideas. 
+       Band 7.0: Fully addresses task, clear position throughout, relevant and developed ideas, possible lack of focus. 
+       Band 8.0: Fully addresses task, clear position, relevant and well-developed ideas.
+       Band 9.0: Fully addresses all parts of task, clear and consistent position, relevant and highly developed ideas, strong conclusion.
+       5.After checking parts put overall mark.Calculate by summing scores and dividing by 4.The overall score is then rounded to the nearest whole or half band. 
+       6.Output only a number of overall score
         """
 
         with get_openai_callback() as cb:
